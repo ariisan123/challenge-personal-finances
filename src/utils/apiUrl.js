78bec2dbd83007fbe75a1879560eb43e301aacd7
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-13 16:25:27 -0300

    api utils

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,23 @@
+const API_PORT = 8000;
+const DB_HOST = 'localhost';
+
+export const endpoints = {
+  login: '/api/login',
+  signup: '/api/signup',
+  opNew: '/api/operations/new',
+  opDelete(id) {
+    return `/api/operations/${id}`;
+  },
+  opEdit(id) {
+    return `/api/operations/${id}`;
+  },
+  operations(endpoint) {
+    return `/api/operations/${endpoint}`;
+  }
+};
+
+export const url = `http://${DB_HOST}:${API_PORT}`;
+
+export const setUrl = (endpoint) => {
+  return `${url}${endpoint}`;
+};
